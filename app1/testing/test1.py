4faf4cf7070cc6733c05015c2a67eaa27cdd80cc
boris-szl <boris.szelcsanyi@outlook.com>
2022-05-15 19:02:40 +0200

    significant improvements made

--- app1/testing/test1.py
+++ app1/testing/test1.py
@@ -11,7 +11,8 @@ import json
 # from company import Company
 sys.path.insert(0, '..')
 from modules import findata as fd
-from modules import company as cd
+from modules import company
+from modules import manipulate
 
 def inputTicker():
 	while True:
@@ -23,8 +24,23 @@ def inputTicker():
 			break
 	return x.upper()
 
-def main():
+def to_dict(df):
+    data = []
+    df = setDateRows1(df)
+    df = df.reset_index()
+    df = df.set_axis(["Dates", "ROIC"], axis=1, inplace=False)
+    dict_data = []
+    for i in range(df.shape[0]):
+        dict_data.append(dict(df.iloc[i]))
+    return df
 
+def toDict(df):
+    dict_data = []
+    for i in range(df.shape[0]):
+        dict_data.append(dict(df.iloc[i]))
+    return dict_data
+
+def main():
 	# x = inputTicker()
 	# print(x)
 	# company = cd.Company(x)
@@ -35,18 +51,17 @@ def main():
 
 	# print(yearly_roic)
 	# print(quarterly_roic)
-
 	# # inaccuarate because it takes the current wacc
 	# print(pd.Series([fd.calculateWacc(year=dt.date.today().year-x) for x in [0,1,2,3]]))
 	# print(yearly_roic - wacc)
 
 	# cleaning the mess up in findata modules
-	date = dt.datetime.now().year
-	print(str(date))
-	URL = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/TextView?type=daily_treasury_long_term_rate&field_tdr_date_value="
-	print(URL + str(date))
-	URL2 = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/TextView?type=daily_treasury_long_term_rate&field_tdr_date_value=" + str(date)
-	print(URL2)
+	# date = dt.datetime.now().year
+	# print(str(date))
+	# URL = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/TextView?type=daily_treasury_long_term_rate&field_tdr_date_value="
+	# print(URL + str(date))
+	# URL2 = "https://home.treasury.gov/resource-center/data-chart-center/interest-rates/TextView?type=daily_treasury_long_term_rate&field_tdr_date_value=" + str(date)
+	# print(URL2)
 	# print(fd.getRiskFreeRateMonth())
 	# print(fd.getRiskFreeRateUs())
 	# print(fd.getCurrentRiskFreeRateUs())
@@ -58,11 +73,42 @@ def main():
 	# print(fd.getUnleveredBeta().iloc[[1]])
 	# print(fd.getUnleveredCleanedBeta())
 	# print(fd.getRiskFreeRateForAustria())
-	print(fd.getRiskFreeRateEuroArea("DE", "EUR"))
-	print(fd.getRiskFreeRateEuroArea("AT", "EUR"))
+	# print(fd.getRiskFreeRateEuroArea("DE", "EUR"))
+	# print(fd.getRiskFreeRateEuroArea("AT", "EUR"))
+
+	target = company.Company("MSFT")
+	print(target)
+	print(target.getNopat())
+	# print(target.getInvestedCapital())
+	# print(target.getNetWorkingCapital())
+	print(target.getEarnings())
+	print("-----------------------------")
+	print("Nopat:")
+	print(target.getNopat())
+	print("-----------------------------")
+	print("Revenue:")
+	print(target.getRevenue())
+	print("-----------------------------")
+	print("Nopat marigin:")
+	print(target.calculateNopatMargin())
+	print("-----------------------------")
+	print(target.calculateCapitalTurnover())
+	print("-----------------------------")
+	print(target.calculateRoicByMultiplication())
+	print("-----------------------------")
+	roic = target.calculateROIC()
+	print(type(roic))
+	print(target.getRevenue().reset_index()["Year"])
+	list_year = list(target.getRevenue().reset_index()["Year"])
+	print(pd.DataFrame(data=roic, dtype=np.float32))
+	print(pd.DataFrame(roic, columns=["ROIC"]))
+	print(pd.DataFrame(list_year, columns=["Dates"]))
 
 
-
+	df_output = pd.DataFrame(data={"Dates": list_year, "ROIC": roic, "Nopat Margin" : target.calculateNopatMargin(), "Capital Turnover" : target.calculateCapitalTurnover()})
+	print(df_output)
+	print(toDict(df_output))
 
 if __name__ == "__main__":
 	main()
+
